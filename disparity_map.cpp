123a2502e097ae2ce9d8ad70092f34bc93ce0763
Rafael Grillo <grillorafael@gmail.com>
2014-10-17 11:23:33 -0300

    fixes

--- disparity_map.cpp
+++ disparity_map.cpp
@@ -8,13 +8,13 @@
 
 std::string IMAGES_PATH =  "/Users/rafael/Projects/python-mosaic/stereo/";
 
-cv::Mat image0 = cv::imread(IMAGES_PATH + "im0mini.png", CV_LOAD_IMAGE_GRAYSCALE);
-cv::Mat image1 = cv::imread(IMAGES_PATH + "im1mini.png", CV_LOAD_IMAGE_GRAYSCALE);
+cv::Mat image0 = cv::imread(IMAGES_PATH + "tsukuba0.png", CV_LOAD_IMAGE_GRAYSCALE);
+cv::Mat image1 = cv::imread(IMAGES_PATH + "tsukuba1.png", CV_LOAD_IMAGE_GRAYSCALE);
 
 cv::Mat addWindowFrames(cv::Mat image);
 cv::Point getBestMatch(cv::Point currentPosition);
-float ssdValue(cv::Point position);
-float getValue(std::string method, cv::Point position);
+double ssdValue(cv::Point position);
+double getValue(std::string method, cv::Point position);
 float distanceBetween(cv::Point p1, cv::Point p2);
 
 cv::Mat addWindowFrames(cv::Mat image) {
@@ -30,8 +30,8 @@ cv::Mat addWindowFrames(cv::Mat image) {
 
 	for(row = 0; row < s.height; row++) {
 		for(column = 0; column < s.width; column++) {
-			uchar color = image.at<uchar>(row, column);
-			output.at<uchar>(row + WINDOW_SIZE, column + WINDOW_SIZE) = color;
+			int color = image.at<int>(row, column);
+			output.at<int>(row + WINDOW_SIZE, column + WINDOW_SIZE) = color;
 		}
 	}
 
@@ -40,31 +40,36 @@ cv::Mat addWindowFrames(cv::Mat image) {
 
 cv::Point getBestMatch(cv::Point currentPosition) {
 	cv::Point bestMatch;
-	float bestValue = -INFINITY;
+	double bestValue = std::numeric_limits<double>::max();
+
 	cv::Size imageSize = image0.size();
-	int column;
-	for(column = 0; column < imageSize.width; column++) {
-		float value = getValue("ssd", cv::Point(column, currentPosition.y));
-		if(value > bestValue) {
+	for(int column = WINDOW_SIZE; column < imageSize.width - WINDOW_SIZE; column++) {
+		double value = getValue("ssd", cv::Point(column, currentPosition.y));
+		if(value < bestValue) {
 			bestValue = value;
 			bestMatch = cv::Point(column, currentPosition.y);
 		}
 	}
+
 	return bestMatch;
 }
 
-float ssdValue(cv::Point position) {
-	float value = 0;
+double ssdValue(cv::Point position) {
+	double value = 0;
 	int row, column;
-	for(row = position.y; row < position.y + WINDOW_SIZE; row++) {
-		for(column = position.x; column < position.x + WINDOW_SIZE; column++) {
-			value += pow(image0.at<uchar>(row, column) - image1.at<uchar>(row, column), 2);
+	int fromX = position.x - (WINDOW_SIZE / 2);
+	int fromY = position.y - (WINDOW_SIZE / 2);
+
+	for(row = fromY; row < fromY + WINDOW_SIZE; row++) {
+		for(column = fromX; column < position.x + WINDOW_SIZE; column++) {
+			value += pow(image0.at<int>(row, column) - image1.at<int>(row, column), 2.0);
 		}
 	}
+
 	return value;
 }
 
-float getValue(std::string method, cv::Point position) {
+double getValue(std::string method, cv::Point position) {
 	if(method == "ssd") {
 		return ssdValue(position);
 	}
@@ -86,35 +91,56 @@ int main(int argc, char** argv) {
 
 	image0 = addWindowFrames(image0);
 	image1 = addWindowFrames(image1);
-
+	
 	cv::Size outputSize = image0.size();
 	cv::Mat output = cv::Mat::zeros(outputSize.height, outputSize.width, CV_8UC1);
-	int row, column;
+	
+	int row;
+	int column;
+	int minValue = 0;
+	int maxValue = 0;
 
-    std::cout << "Calculating disparity map...\n";
-
-	float biggest = 0;
+	std::cout << "Calculating disparity map...\n";
 	for(row = WINDOW_SIZE; row < outputSize.height - WINDOW_SIZE; row++) {
         std::cout << "\r" << ((row * 100) / outputSize.height) << "% ";
         std::cout.flush();
+		
 		for(column = WINDOW_SIZE; column < outputSize.width - WINDOW_SIZE; column++) {
+			
 			cv::Point currentPosition = cv::Point(column, row);
 			cv::Point bestMatch = getBestMatch(currentPosition);
 			float disparity = distanceBetween(currentPosition, bestMatch);
-			output.at<uchar>(row + WINDOW_SIZE, column + WINDOW_SIZE) = disparity;
-			if(disparity > biggest) {
-				biggest = disparity;
+			output.at<int>(row, column) = disparity;
+//			std::cout << "\nDisparity Found: " << disparity << " Image value: " << output.at<int>(row, column);
+
+			if(disparity > maxValue) {
+				maxValue = disparity;
+			}
+			if(disparity < minValue) {
+				minValue = disparity;
 			}
 		}
 	}
 
-	// output = output / biggest;
-	// output = output * 255;
-
+	std::cout << "\nImage min value: " << minValue << "\nImage max value: " << maxValue << "\n";
+	
+	
+	// Normalizing image
+	for (row = WINDOW_SIZE; row < (outputSize.height - WINDOW_SIZE); row++) {
+		for (column = WINDOW_SIZE; column < (outputSize.width - WINDOW_SIZE); column++) {
+			float value = (((float)output.at<int>(row, column) - (float)minValue) / (float)maxValue) * 255.0;
+//			std::cout << "\nImage old value: " << output.at<int>(row, column) << " Image new Value: " << value;
+			output.at<int>(row, column) = (int)value;
+		}
+	}
+	std::cout << "\n";
+	// End image normalization
 
 	clock_t end = clock();
 	double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
-	std::printf("%f Seconds", elapsed_secs);
+	std::cout << "It took " << elapsed_secs << " seconds";
+	
+	cv::imwrite("out/disparity_map.png", output);
 
 	cv::namedWindow("Gray image", CV_WINDOW_AUTOSIZE);
 	cv::imshow("Gray image", output);
