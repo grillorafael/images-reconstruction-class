ad0edae0ea92170abc096922bc986a625f0d80b7
Rafael Grillo <grillorafael@gmail.com>
2014-10-20 20:27:33 -0200

    adjusting compilation script

--- disparity_map.sh
+++ disparity_map.sh
@@ -1,4 +1,1 @@
-# clang++ -std=c++11 disparity_map.cpp `pkg-config --cflags --libs opencv`
-rm ./a.out
 g++ disparity_map.cpp -O3 `pkg-config --cflags --libs opencv`
-# g++ disparity_map.cpp `pkg-config --cflags --libs opencv`
