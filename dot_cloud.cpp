561cc8b6bf8a98f4417d23270d97cfb2ba1cf04d
Rafael Grillo <grillorafael@gmail.com>
2014-11-20 00:23:51 -0200

    search for corresponding point

--- dot_cloud.cpp
+++ dot_cloud.cpp
@@ -7,6 +7,7 @@
 #include <Eigen/Dense>
 #include <gsl/gsl_poly.h>
 
+#define WINDOW_SIZE 9
 #define ARRAY_SIZE(array) (sizeof((array))/sizeof((array[0])))
 
 std::string IMAGES_PATH =  "/Users/rafael/Projects/python-mosaic/fm/";
@@ -63,36 +64,90 @@ double r1[3][3] = {
 double t0[3] = {-0.05157154578564796000, 0.00120001566069944090, 0.60066423290325455000};
 double t1[3] = {-0.04592612096202596000, -0.00032656373638871063, 0.60909066773860632000};
 
-// 8 points algorithm normalized
-cv::Mat getNormalizedFundamentalMatrix(cv::Point* points1, cv::Point* points2) {
-	std::cout << "\n" << "[getNormalizedFundamentalMatrix] Starting" << "\n";
-	cv::Mat s, u, vt, F, t, T, P;
-	cv::Mat A = cv::Mat::zeros(8, 9, CV_64F);
+cv::Mat addWindowFrames(cv::Mat image) {
+	cv::Size s = image.size();
 	
-	cv::Size im0size = image0.size();
-	cv::Size im1size = image1.size();
+	std::cout << "[addWindowFrames] Image Height " << s.height << "\n";
+	std::cout << "[addWindowFrames] Image Width " << s.width << "\n";
 	
-	double tTmp[3][3] = {
-		{ im0size.width + im0size.height, 0, im0size.width / 2 },
-		{ 0, im0size.width + im0size.height, im0size.height / 2 },
-		{ 0, 0, 1 },
-	};
-	t = cv::Mat(3, 3, CV_64F, &tTmp);
+	cv::Mat output = cv::Mat::zeros(s.height + 2 * WINDOW_SIZE, s.width + 2 * WINDOW_SIZE, CV_8UC3);
 	
-	double TTmp[3][3] = {
-		{ im1size.width + im1size.height, 0, im1size.width / 2 },
-		{ 0, im1size.width + im1size.height, im1size.height / 2 },
-		{ 0, 0, 1 },
-	};
-	T = cv::Mat(3, 3, CV_64F, &TTmp);
+	int column;
+	int row;
 	
-	for (int i = 0; i < 8; i+=1) {
-		
+	for(row = 0; row < s.height; row++) {
+		for(column = 0; column < s.width; column++) {
+			cv::Vec3b color = image.at<cv::Vec3b>(row, column);
+			output.at<cv::Vec3b>(row + WINDOW_SIZE, column + WINDOW_SIZE) = color;
+		}
 	}
 	
-	
-	return A;
+	return output;
 }
+
+double distanceBetween(cv::Point p1, cv::Point p2) {
+	int x1 = p1.x;
+	int x2 = p2.x;
+	
+	int y1 = p1.y;
+	int y2 = p2.y;
+	
+	return sqrt(pow((x2 - x1), 2) + pow((y2 - y1), 2));
+}
+
+double ssdValue(cv::Point currentPosition, cv::Point position) {
+	double value = 0;
+	int row, column;
+	int fromX = -WINDOW_SIZE;
+	int fromY = -WINDOW_SIZE;
+	cv::Point from, to;
+	
+	for(row = -WINDOW_SIZE; row <= WINDOW_SIZE; row++) {
+		for(column = -WINDOW_SIZE; column <= WINDOW_SIZE; column++) {
+			cv::Vec3b image0Value = image0.at<cv::Vec3b>(currentPosition.y + row, currentPosition.x + column);
+			cv::Vec3b image1Value = image1.at<cv::Vec3b>(position.y + row, position.x + column);
+			
+			from = cv::Point(image0Value[1], image0Value[2]);
+			to = cv::Point(image1Value[1], image1Value[2]);
+			
+			
+			value += pow(distanceBetween(from, to), 2.0);
+		}
+	}
+	
+	return value;
+}
+
+// 8 points algorithm normalized
+//cv::Mat getNormalizedFundamentalMatrix(cv::Point* points1, cv::Point* points2) {
+//	std::cout << "\n" << "[getNormalizedFundamentalMatrix] Starting" << "\n";
+//	cv::Mat s, u, vt, F, t, T, P;
+//	cv::Mat A = cv::Mat::zeros(8, 9, CV_64F);
+//	
+//	cv::Size im0size = image0.size();
+//	cv::Size im1size = image1.size();
+//	
+//	double tTmp[3][3] = {
+//		{ im0size.width + im0size.height, 0, im0size.width / 2 },
+//		{ 0, im0size.width + im0size.height, im0size.height / 2 },
+//		{ 0, 0, 1 },
+//	};
+//	t = cv::Mat(3, 3, CV_64F, &tTmp);
+//	
+//	double TTmp[3][3] = {
+//		{ im1size.width + im1size.height, 0, im1size.width / 2 },
+//		{ 0, im1size.width + im1size.height, im1size.height / 2 },
+//		{ 0, 0, 1 },
+//	};
+//	T = cv::Mat(3, 3, CV_64F, &TTmp);
+//	
+//	for (int i = 0; i < 8; i+=1) {
+//		
+//	}
+//	
+//	
+//	return A;
+//}
 
 // 8 points algorithm
 cv::Mat getFundamentalMatrix(cv::Point* points1, cv::Point* points2) {
@@ -171,77 +226,53 @@ cv::Mat getPMatrix(double k[3][3], double r[3][3],double t[3]) {
 	return p;
 }
 
-cv::Point get3dPoint(cv::Mat F, cv::Point x, cv::Point X) {
-	std::cout << "\n" << "[get3dPoint] Starting" << "\n";
-	
-	cv::Mat u, s, vt, e, E, r, R;
-	double a, b, c, d, fe, fl;
-	
-	cv::Mat px = cv::Mat(3, 1, CV_64F);
-	px.at<double>(0, 0) = x.x;
-	px.at<double>(1, 0) = x.y;
-	px.at<double>(2, 0) = 1;
-	
-	cv::Mat pX = cv::Mat(3, 1, CV_64F);
-	px.at<double>(0, 0) = X.x;
-	px.at<double>(1, 0) = X.y;
-	px.at<double>(2, 0) = 1;
-	
-	double tTmp[3][3] = {
-		{1, 0, -x.x},
-		{0, 1, -x.y},
-		{0, 0, 1},
-	};
-	cv::Mat t = cv::Mat(3, 3, CV_64F, &tTmp);
-	
-	double TTmp[3][3] = {
-		{1, 0, -X.x},
-		{0, 1, -X.y},
-		{0, 0, 1},
-	};
-	cv::Mat T = cv::Mat(3, 3, CV_64F, &TTmp);
-	
-	px = t * px;
-	px = px / px.at<double>(2);
-	
-	pX = T * pX;
-	pX = pX / pX.at<double>(2);
-	
-	F = T.t().inv() * F * t.inv();
-	cv::SVD::compute(F, u, s, vt, cv::SVD::FULL_UV);
-	
-	e = vt.col(2);
-	E = u.row(2);
+cv::Mat get3dPoint(cv::Mat F, cv::Mat x) {
+	cv::Point ptX = cv::Point(x.at<double>(0, 0), x.at<double>(1, 0));
+	cv::Point pt1 = cv::Point(0, 0);
+	cv::Point pt2 = cv::Point(0, 0);
 
-	e = e / sqrt((pow(vt.at<double>(1, 3), 2))+(pow(vt.at<double>(2, 3), 2)));
-	E = E / sqrt((pow(u.at<double>(3, 1), 2))+(pow(u.at<double>(3, 2), 2)));
+	cv::Mat homog = cv::Mat::zeros(3, 1, CV_64FC1);
+	homog.at<double>(2, 0) = 1;
 	
-	double rTmp[3][3] = {
-		{e.at<double>(0, 0), e.at<double>(1, 0), 0},
-		{-e.at<double>(1, 0), e.at<double>(0, 0), 0},
-		{0, 0, 1},
-	};
-	r = cv::Mat(3, 3, CV_64F, &rTmp);
+	cv::Mat rightUpper = cv::Mat::zeros(3, 1, CV_64FC1);
+	rightUpper.at<double>(0, 0) = image0.size().width;
+	rightUpper = homog.cross(rightUpper);
 	
-	double RTmp[3][3] = {
-		{E.at<double>(0, 0), E.at<double>(1, 0), 0},
-		{-E.at<double>(1, 0), E.at<double>(0, 0), 0},
-		{0, 0, 1},
-	};
-	R = cv::Mat(3, 3, CV_64F, &RTmp);
+	cv::Mat leftDown = cv::Mat::zeros(3, 1, CV_64FC1);
+	leftDown.at<double>(1, 0) = image0.size().height;
+	leftDown = homog.cross(leftDown);
 	
-	F = R.t().inv() * F * r.inv();
+	cv::Mat line = F * x;
 	
-	a = F.at<double>(2, 2);
-	b = F.at<double>(2, 3);
-	c = F.at<double>(3, 2);
-	d = F.at<double>(3, 3);
-	fe = e.at<double>(3, 1);
-	fl = E.at<double>(3, 1);
+	cv::Mat p1 = rightUpper.cross(line);
+	p1 = p1/p1.at<double>(2, 0);
+	pt1.x = p1.at<double>(0, 0);
+	pt1.y = p1.at<double>(1, 0);
 	
-	double coefs[7]
+	cv::Mat p2 = rightUpper.cross(line);
+	p2 = p2 / p2.at<double>(2, 0);
+	pt2.x = p2.at<double>(0, 0);
+	pt2.y = p2.at<double>(1, 0);
 	
-	return X;
+	cv::LineIterator it(image1, pt1, pt2, 8);
+	
+	double bestValue = std::numeric_limits<double>::max();;
+	cv::Point bestMatch;
+	
+	//	KEEPS RETURNING 0
+	std::cout << "\n" << it.count << "\n";
+	for(int i = 0; i < it.count; i++, ++it) {
+		if(it.pos().x < WINDOW_SIZE && it.pos().y > WINDOW_SIZE) {
+			double value = ssdValue(ptX, it.pos());
+			if(value < bestValue) {
+				bestValue = value;
+				bestMatch = it.pos();
+			}
+		}
+	}
+	
+	return line;
+	
 }
 
 int main() {
@@ -251,16 +282,28 @@ int main() {
 	std::cout << "\n" << "[main] Initializing variables";
 	cv::Mat p0,p1,f;
 	
+	cvtColor(image0, image0, CV_BGR2Lab);
+	cvtColor(image1, image1, CV_BGR2Lab);
+	
+	image0 = addWindowFrames(image0);
+	image1 = addWindowFrames(image1);
+	
 	std::cout << "\n" << "[main] Calculating variables" << "\n";
 	f = getFundamentalMatrix(image0Points, image1Points);
-//	f = getNormalizedFundamentalMatrix(image0Points, image1Points);
 
 	p0 = getPMatrix(k0, r0, t0);
 	p1 = getPMatrix(k1, r1, t1);
 	
 	
-	for (int i = 0; i < 8; i++) {
-		std::cout << "\n" << get3dPoint(f, image0Points[i], image1Points[i]) << "\n";
+	for (int x = 0; x < image0.size().width; x++) {
+		for (int y = 0; y < image0.size().height; y++) {
+			// TODO: SKIP BLACK PIXELS
+			cv::Mat p = cv::Mat::zeros(3, 1, CV_64FC1);
+			p.at<double>(0, 0) = x;
+			p.at<double>(1, 0) = y;
+			p.at<double>(2, 0) = 1;
+			get3dPoint(f, p);
+		}
 	}
 	
 	
